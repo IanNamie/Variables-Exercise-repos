fa7df8e7dfe5f1fe6779abc0ce954bb8304d90d5
Ian Namie <Iannamie82@gmail.com>
2023-01-24 10:40:09 -0600

    fixed grammer mistake

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -7,13 +7,13 @@
             Console.WriteLine("Hello, World!");
             string catsName = "Rosie";
             string catsOtherName = "Rosie Posey";
-            int catsAge = 3;
+            int catsAge = 4;
             char catsInitals = 'R';
             bool cuteCat = true;
             Double catsWeight = 10.5;
             decimal catNaps = 10.5m;
 
-            Console.WriteLine($"My cats name is {catsName}, we sometime call her {catsOtherName}. " +
+            Console.WriteLine($"My cats name is {catsName}, we sometimes call her {catsOtherName}. " +
                 $"She is {catsAge} years old!");
         }
     }
