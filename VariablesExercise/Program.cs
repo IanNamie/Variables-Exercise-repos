fac7384773a98b5f16bd29cc01fb3fa8f61d8016
Ian Namie <Iannamie82@gmail.com>
2023-01-24 10:34:48 -0600

    Made sentence about my cat.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string catsName = "Rosie";
+            string catsOtherName = "Rosie Posey";
+            int catsAge = 3;
+            char catsInitals = 'R';
+            bool cuteCat = true;
+            Double catsWeight = 10.5;
+            decimal catNaps = 10.5m;
+
+            Console.WriteLine($"My cats name is {catsName}, we sometime call her {catsOtherName}. " +
+                $"She is {catsAge} years old!");
         }
     }
 }
